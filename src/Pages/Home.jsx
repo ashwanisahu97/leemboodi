aa392e9921f8283ca774e1970bb45e9fcfbe8139
Ashwani Sahu <ashwani.s@honeysys.com>
2024-04-19 10:36:21 +0530

    Body section:slideshow portion started

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../mainSections/Navbar'
+import Body from '../mainSections/Body'
 
 const Home = () => {
   return (
     <>
         <Navbar/>
+        <Body/>
     </>
   )
 }
--- src/mainSections/Body.jsx
+++ src/mainSections/Body.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import "../Styles/Body/body.scss";
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className='mainContainer'>
+     <div className="banner">
+      <div className="left"></div>
+      <div className="right"></div>
+     </div>
+    </div>
   )
 }
 
