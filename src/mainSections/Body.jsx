aab336b46ed7a73e58f881f9ca02fd1fce168397
Ashwani Sahu <ashwani.s@honeysys.com>
2024-05-14 15:36:00 +0530

    banner part implementation

--- src/mainSections/Body.jsx
+++ src/mainSections/Body.jsx
@@ -4,8 +4,12 @@ const Body = () => {
   return (
     <div className='mainContainer'>
      <div className="banner">
-      <div className="left"></div>
-      <div className="right"></div>
+      <div className="left">
+        <img src="https://leemboodi.com/cdn/shop/files/Ethnic-Banner---Desktop_200.jpg?v=1696678012&width=2000" alt="" />
+      </div>
+      <div className="right">
+        <img src="https://leemboodi.com/cdn/shop/files/Western-Banner-Desktop_200.jpg?v=1696678019&width=2000" alt="" />
+      </div>
      </div>
     </div>
   )
